9ce9ed17262b1ae0d3c7ab03d5fd9a034acfdc08
JulioGuevaraA01704733 <88898418+JulioGuevaraA01704733@users.noreply.github.com>
2021-10-21 23:51:54 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,18 +1,18 @@
 def IMC (peso1, estatura1):
-''' 
-Recibe el peso en kilogramos y la estatura en metros del usuario. Realiza la operación aritmética para la obtención del número que llamamos IMC y devuelve dicho valor.
-Las siglas IMC representan Índice de Masa Corporal, valor de gran utilidad para el análisis de la salud.
-'''
+    """
+    Recibe el peso en kilogramos y la estatura en metros del usuario. Realiza la operación aritmética para la obtención del número que llamamos IMC y devuelve dicho valor.
+    Las siglas IMC representan Índice de Masa Corporal, valor de gran utilidad para el análisis de la salud.
+    """
     return peso1 / (estatura1*estatura1)
 
 def IMC_ideal (edad1, sexo1):
-'''
-Recibe la edad y sexo del usuario -éste último se da en string-, ya que existen dos bases de referencia para determinar el IMC ideal, una para cada sexo. 
-Existen dos rangos en los que es indiferente el sexo, los cuales se ven representados en los primeros condicionales de la función.
-Si ninguno de los dos es el caso, se manipula aritmeticamente el valor de la edad para que, al sumarla con los rangos iniciales,
-resulte en el valor superior e inferior (los cuales varían dependiendo del sexo) del rango correspondiente. Se resuelve para las excepciones con una estructura más
-de condiciones. Finalmente, la función regresa el rango dentro del cual se encuentra el IMC ideal del usuario.
-'''
+    """
+    Recibe la edad y sexo del usuario -éste último se da en string-, ya que existen dos bases de referencia para determinar el IMC ideal, una para cada sexo. 
+    Existen dos rangos en los que es indiferente el sexo, los cuales se ven representados en los primeros condicionales de la función.
+    Si ninguno de los dos es el caso, se manipula aritmeticamente el valor de la edad para que, al sumarla con los rangos iniciales,
+    resulte en el valor superior e inferior (los cuales varían dependiendo del sexo) del rango correspondiente. Se resuelve para las excepciones con una estructura más
+    de condiciones. Finalmente, la función regresa el rango dentro del cual se encuentra el IMC ideal del usuario.
+    """
     if edad1 <= 15 :
         return "Dado que estás en una etapa de desarrollo, tu IMC ideal no es establecible."
     elif edad1 >= 65 :
@@ -41,23 +41,23 @@ de condiciones. Finalmente, la función regresa el rango dentro del cual se encu
 
 #TMC representa Tasa Metabólica Basal
 def TMC(peso2, estatura2, edad2, sexo2):
-'''
-Esta función recibe el peso, la estatura, edad y sexo del usuario. Dependiendo del sexo, decide con qué valores llevar a cabo una operación matemática (la 
-cual emplea las demás variables que recibió la función) que resulta en una aproximación a la cantidad de calorías diarias consumidas por el usuario. 
-EL valor de esta operación es lo que devuelve la función.
-'''
+    """
+    Esta función recibe el peso, la estatura, edad y sexo del usuario. Dependiendo del sexo, decide con qué valores llevar a cabo una operación matemática (la 
+    cual emplea las demás variables que recibió la función) que resulta en una aproximación a la cantidad de calorías diarias consumidas por el usuario. 
+    EL valor de esta operación es lo que devuelve la función.
+    """
     if sexo2 == "mujer" or sexo2 == "Mujer" :
         return 655 + 9.6 * peso2 + 180 * estatura2 - 4.7 * edad2
     else :
         return 66 + 13.7 * peso2 + 500 * estatura2 - 6.75 * edad2
     
 def TMC2(peso2, estatura2, edad2, sexo2, ejercicio):
-'''
-Esta función mejora la aproximación realizada por la función TMC empleando una variable extra. La función recibe el valor que regresó la variable TMC (por lo que
-podría decirse que también recibe peso, estatura, edad y sexo) y la variable que representa el nivel de ejercicio que realiza el usuario, el cual se describe a través
-de números enteros del 1 al 5, cada uno correspondiente a una descripción particular de la cantidad de ejercicio que se realiza. Con ayuda de una estructura condicional, 
-la función decide cuánto aumentar el valor regresado por la función TMC, con base en el nivel de ejercicio que se indicó.
-'''
+    """
+    Esta función mejora la aproximación realizada por la función TMC empleando una variable extra. La función recibe el valor que regresó la variable TMC (por lo que
+    podría decirse que también recibe peso, estatura, edad y sexo) y la variable que representa el nivel de ejercicio que realiza el usuario, el cual se describe a través
+    de números enteros del 1 al 5, cada uno correspondiente a una descripción particular de la cantidad de ejercicio que se realiza. Con ayuda de una estructura condicional, 
+    la función decide cuánto aumentar el valor regresado por la función TMC, con base en el nivel de ejercicio que se indicó.
+    """
     if ejercicio == 1:
         return (TMC(peso2, estatura2, edad2, sexo2) * 1.2)
     elif ejercicio == 2 :
@@ -74,14 +74,14 @@ lista_porras = ["¡Échale ganas, haz ejercicio!", "¡Sigue esforzándote!", "¡
 lista_com = [lista_consejos, lista_porras]
 
 def comentario(decision1, nivel_e):
-'''
-Esta función hace uso de un string que da el usuario al ser hecho decidir entre dos opciones y de la variable que representa el nivel de ejercicio que hace
-el usuario, igual que en la función TMC2 (Estas son las variables que recibe). Dependiendo de lo que haya escrito el usuario, la función le asigna un valor 
-u otro a la variable que posteriormente se usará para describir a qué sublista llamar, dentro de la lista_com, la cual se ve compuesta por una lista de consejos
-y otra de porras. Al valor de la variable nivel de ejercicio se le resta uno y el resultado se designa a una nueva variable, para que ésta se alinee con las características
-de los índices de las listas, y se utilice para llamar al elemento correcto de la sublista correspondiente, ya que cada consejo o porra está en orden respectivo
-al nivel de ejercicio. Regresa el elemento correspondiente al nivel de ejercicio de la lista correspondiente a la desición.
-'''
+    """
+    Esta función hace uso de un string que da el usuario al ser hecho decidir entre dos opciones y de la variable que representa el nivel de ejercicio que hace
+    el usuario, igual que en la función TMC2 (Estas son las variables que recibe). Dependiendo de lo que haya escrito el usuario, la función le asigna un valor 
+    u otro a la variable que posteriormente se usará para describir a qué sublista llamar, dentro de la lista_com, la cual se ve compuesta por una lista de consejos
+    y otra de porras. Al valor de la variable nivel de ejercicio se le resta uno y el resultado se designa a una nueva variable, para que ésta se alinee con las características
+    de los índices de las listas, y se utilice para llamar al elemento correcto de la sublista correspondiente, ya que cada consejo o porra está en orden respectivo
+    al nivel de ejercicio. Regresa el elemento correspondiente al nivel de ejercicio de la lista correspondiente a la desición.
+    """
     nivel_t = nivel_e - 1 
     if decision1 == "porra" or decision1 == "Porra" or decision1 == "porras" or decision1 == "Porras" or decision1 == "PORRAS" :
         pos_com = 1
@@ -91,11 +91,11 @@ al nivel de ejercicio. Regresa el elemento correspondiente al nivel de ejercicio
     return ""
 
 def rutina(nivel):
-'''
-Esta variable recibe el nivel de ejercicio del usuario, para con este decidir si recomendar una rutina de ejercicios. La variable también se usa para determinar
-cuantas repeticiones se harán y cuántos minutos de cardio. Si el nivel de ejercicio es menor a 4, la función regresa una tabla con 6 días de la semana y la cantidad
-de ejercicio a hacer en cada uno. Se usan ciclos while para repetir el proceso hasta completar la tabla.
-'''
+    """
+    Esta variable recibe el nivel de ejercicio del usuario, para con este decidir si recomendar una rutina de ejercicios. La variable también se usa para determinar
+    cuantas repeticiones se harán y cuántos minutos de cardio. Si el nivel de ejercicio es menor a 4, la función regresa una tabla con 6 días de la semana y la cantidad
+    de ejercicio a hacer en cada uno. Se usan ciclos while para repetir el proceso hasta completar la tabla.
+    """
     if nivel < 4 :
         dia = 1
         tiempo = 15 * nivel
